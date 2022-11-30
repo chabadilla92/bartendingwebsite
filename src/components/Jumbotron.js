f9941c9cb69455f1c139dcc05784f02466207be2
Brandon Nguyen <nbrandon62@gmail.com>
2022-11-30 14:54:39 -0800

    got the page title to overlay the jumbo image

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,54 +1,56 @@
 import React from "react";
 import "../css/jumbotron.css";
-import booking from '../images/booking.avif';
-import contact from '../images/contact.avif';
-import faq from '../images/faq.avif';
-import home from '../images/home.avif';
-import services from '../images/services.avif';
-
+import booking from "../images/booking.avif";
+import contact from "../images/contact.avif";
+import faq from "../images/faq.avif";
+import home from "../images/home.avif";
+import services from "../images/services.avif";
 
 const jumbotronData = [
   {
-    id: '/',
-    img: <img className="ui fluid image" src={home} alt=""/>,
+    id: "/",
+    title: "welcome",
+    img: <img src={home} alt="" />,
   },
   {
-    id: '/booking',
-    title: 'book now',
-    img: <img className="ui fluid image" src={booking} alt=""/>
+    id: "/booking",
+    title: "book now",
+    img: <img src={booking} alt="" />,
   },
   {
-    id: '/contact',
-    title: 'contact us',
-    img: <img className="ui fluid image" src={contact} alt=""/>
+    id: "/contact",
+    title: "contact us",
+    img: <img src={contact} alt="" />,
   },
   {
-    id: '/questions',
-    title: 'faq',
-    img: <img className="ui fluid image" src={faq} alt=""/>
+    id: "/questions",
+    title: "faq",
+    img: <img src={faq} alt="" />,
   },
   {
-    id: '/services',
-    title: 'services',
-    img: <img className="ui fluid image" src={services} alt=""/>
+    id: "/services",
+    title: "services",
+    img: <img src={services} alt="" />,
   },
-]
+];
 
 const Jumbotron = () => {
-  const renderedImage = jumbotronData.map((data) => {
-    if (data.id === window.location.pathname) {
-      return (
-        <div key={data.id}>
-          {data.title}
-          {data.img}
-        </div>
-      );
-    } else {
-      return null;
-    }
-  });
-
-  return <div>{renderedImage}</div>;
+  return (
+    <div>
+      {jumbotronData.map((data) => {
+        if (data.id === window.location.pathname) {
+          return (
+            <div className="jumbotron">
+              <div className="title">{data.title}</div>
+              <div className="ui fluid image">{data.img}</div>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </div>
+  );
 };
 
 export default Jumbotron;
