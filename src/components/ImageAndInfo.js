addc21e99a380341396e4b4db141e7461d9cd5b0
Brandon Nguyen <nbrandon62@gmail.com>
2022-11-30 21:06:12 -0800

    created grid component that holds an image/info container

---
+++ src/components/ImageAndInfo.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import { Image, Grid } from "semantic-ui-react";
+
+const Wrapper = styled.div`
+  margin: 55px;
+  height: 100%;
+  display: flex;
+`;
+
+const Img = styled.div`
+  object-fit: contain;
+`;
+
+const Title = styled.h1`
+  font-size: 20px;
+`;
+const Description = styled.p`
+  font-size: 15px;
+  letter-spacing: 1px;
+`;
+
+const ImageAndInfo = ({ image, title, description }) => {
+  return (
+    <Wrapper>
+      <Grid columns={2} relaxed="very" stackable>
+        <Grid.Column>
+          <Img>
+            <Image src={image} fluid />
+          </Img>
+        </Grid.Column>
+        <Grid.Column>
+          <Title>{title}</Title>
+          <Description>{description}</Description>
+        </Grid.Column>
+      </Grid>
+    </Wrapper>
+  );
+};
+
+export default ImageAndInfo;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,28 @@
 import React from "react";
 import PageHeader from "../components/PageHeader";
+import home from "../images/home.avif";
+import ImageAndInfo from "../components/ImageAndInfo";
 
 const header = {
   title: "Just Chill'd",
-  subheader: "bartending website testing subheader"
-}
+  subheader: "bartending website testing subheader",
+};
+
+const info = {
+  title: "about us",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+};
 
 const Home = () => {
   return (
     <div className="home">
       <PageHeader header={header} />
+      <ImageAndInfo
+        image={home}
+        title={info.title}
+        description={info.description}
+      />
     </div>
   );
 };
