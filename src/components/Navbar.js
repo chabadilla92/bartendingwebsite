81c17c90e9971d23653d85778a3582f6d7ac6e60
Brandon Nguyen <nbrandon62@gmail.com>
2022-11-30 16:09:00 -0800

    css changes for the jumbotron

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Container, Menu, Header, Icon } from "semantic-ui-react";
 
-import "../css/navBar.css"
+import "../css/navBar.css";
 import Jumbotron from "./Jumbotron";
 
 class Navbar extends Component {
-
   state = {};
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
@@ -16,66 +15,65 @@ class Navbar extends Component {
 
     return (
       <div>
+        <Container>
+          <Menu tabular>
+            <Header as="h1">
+              <Icon circular name="beer" />
+            </Header>
+            <Menu.Menu position="right">
+              <Link to="/">
+                <Menu.Item
+                  name="home"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                  className="item"
+                >
+                  home
+                </Menu.Item>
+              </Link>
 
-      <Container>
-        <Menu tabular>
-          <Header as="h1">
-            <Icon circular name="beer" />
-          </Header>
-          <Menu.Menu position="right">
-            <Link to="/">
-              <Menu.Item
-                name="home"
-                active={activeItem === "home"}
-                onClick={this.handleItemClick}
-                className="item"
-              >
-                home
-              </Menu.Item>
-            </Link>
+              <Link to="/contact">
+                <Menu.Item
+                  name="contact"
+                  active={activeItem === "contact"}
+                  onClick={this.handleItemClick}
+                >
+                  contact
+                </Menu.Item>
+              </Link>
 
-            <Link to="/contact">
-              <Menu.Item
-                name="contact"
-                active={activeItem === "contact"}
-                onClick={this.handleItemClick}
-              >
-                contact
-              </Menu.Item>
-            </Link>
+              <Link to="/services">
+                <Menu.Item
+                  name="services"
+                  active={activeItem === "services"}
+                  onClick={this.handleItemClick}
+                >
+                  services
+                </Menu.Item>
+              </Link>
 
-            <Link to="/services">
-              <Menu.Item
-                name="services"
-                active={activeItem === "services"}
-                onClick={this.handleItemClick}
-              >
-                services
-              </Menu.Item>
-            </Link>
+              <Link to="/booking">
+                <Menu.Item
+                  name="book now"
+                  active={activeItem === "book now"}
+                  onClick={this.handleItemClick}
+                >
+                  book now
+                </Menu.Item>
+              </Link>
 
-            <Link to="/booking">
-              <Menu.Item
-                name="book now"
-                active={activeItem === "book now"}
-                onClick={this.handleItemClick}
-              >
-                book now
-              </Menu.Item>
-            </Link>
-
-            <Link to="/questions">
-              <Menu.Item
-                name="faq"
-                active={activeItem === "faq"}
-                onClick={this.handleItemClick}
-              >
-                faq
-              </Menu.Item>
-            </Link>
-          </Menu.Menu>
-        </Menu>
-      </Container>
+              <Link to="/questions">
+                <Menu.Item
+                  name="faq"
+                  active={activeItem === "faq"}
+                  onClick={this.handleItemClick}
+                >
+                  faq
+                </Menu.Item>
+              </Link>
+            </Menu.Menu>
+          </Menu>
+        </Container>
         <Jumbotron />
       </div>
     );
