0c3004eeab8d091e857d22af09f2dc1efbc3c0f1
Brandon Nguyen <nbrandon62@gmail.com>
2022-12-03 17:05:16 -0800

    created an instruction list for the services page using conditional rendering

---
+++ src/components/InstructionItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header, Icon } from "semantic-ui-react";
+
+export const InstructionItem = ({ number, header}) => {
+  return (
+    <Header as="h2" textAlign="center">
+      <Icon circular>
+        {number}
+      </Icon>
+      <Header.Content>{header}</Header.Content>
+    </Header>
+  );
+};
